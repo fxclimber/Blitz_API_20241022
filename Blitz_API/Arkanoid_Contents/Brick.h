0dde3335462c69f6e02b479ddb2a0ed08100b4a1
seo eunjeong <fxclimber@gmail.com>
2024-11-06 01:15:32 +0900

    벽돌 헤더에 타일맵 코드 -직렬화있던거-제거

--- Blitz_API/Arkanoid_Contents/Brick.h
+++ Blitz_API/Arkanoid_Contents/Brick.h
@@ -8,9 +8,8 @@
 #include <EngineCore/EngineCoreDebug.h>
 #include "ContentsEnum.h"
 
-class Brick : public AActor ,public ISerializObject
+class Brick : public AActor
 {
-
 public:
 	Brick();
 	~Brick();
@@ -22,48 +21,9 @@ public:
 		return SpriteRenderer;
 	}
 	FVector2D CheckCollision(const FVector2D& playerPos, const FVector2D& playerSize);
-// 타일하나하나가 개별적인 크기를 가지고 있다면
-// 이걸로 직접 입력해주셔야 합니다.
-	FVector2D Scale;
-	FVector2D Pivot;
-	int SpriteIndex;
-
-	bool IsMove = true;
-	int TileType = -1;
 
 	class USpriteRenderer* SpriteRenderer;
 
-	// 데이터를 직렬화(압축)
-	void Serialize(UEngineSerializer& _Ser)
-	{
-		std::string SpriteName;
-		if (nullptr != SpriteRenderer)
-		{
-			SpriteName = SpriteRenderer->GetCurSpriteName();
-		}
-		_Ser << SpriteName;
-		_Ser << IsMove;
-		_Ser << TileType;
-		_Ser << Scale;
-		_Ser << Pivot;
-		_Ser << SpriteIndex;
-	}
-	void DeSerialize(UEngineSerializer& _Ser)
-	{
-		//std::string SpriteName;
-		// _Ser >> SpriteName;
-
-		// SpriteRenderer->SetSprite(SpriteName);
-
-		std::string SpriteName;
-		_Ser >> SpriteName;
-		_Ser >> IsMove;
-		_Ser >> TileType;
-		_Ser >> Scale;
-		_Ser >> Pivot;
-		_Ser >> SpriteIndex;
-
-	}
 
 protected:
 
